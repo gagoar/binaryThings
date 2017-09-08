8ccfcfea6da1532dfc8a753f183a0d60d320376d
gagoar <xeroice@gmail.com>
2017-09-08 16:08:43 -0700

    adding contains and insert to BST

---
+++ BST/BST.js
@@ -0,0 +1,72 @@
+class BinarySearchTree {
+  constructor(value = null) {
+    this.state = {
+      // a value that can be compared aginst another BST values (ht or lt);
+      value,
+      // another BST where all the values are higher than (ht) the current value.
+      right: null,
+      // another BST where all the values are higher than the current value.
+      left: null,
+    };
+  }
+
+  insert(value) {
+    if(this.value > value) {
+      this.left
+        ? this.left.insert(value)
+        : (this.setLeft(value));
+
+    } else if (this.value < value) {
+      this.right
+        ? this.right.insert(value)
+        : (this.setRight(value));
+    }
+  }
+
+  contains(value) {
+    let found = false;
+
+    if(this.value > value) {
+      found = this.left
+        ? this.left.contains(value)
+        : false;
+
+    } else if (this.value < value) {
+      found = this.right
+        ? this.right.contains(value)
+        : false;
+    }
+
+    if (this.value === value) {
+      found = true;
+    }
+
+    return found;
+  }
+
+  set value(value) {
+    this.state.value = value;
+  }
+
+  get value() {
+    return this.state.value;
+  }
+
+  setRight(value) {
+    this.state.right = new BinarySearchTree(value);
+  }
+
+  get right() {
+    return this.state.right;
+  }
+
+  setLeft(value) {
+    this.state.left = new BinarySearchTree(value);
+  }
+
+  get left() {
+    return this.state.left;
+  }
+}
+
+module.exports = BinarySearchTree;
---
+++ BST/BST.test.js
@@ -0,0 +1,77 @@
+'use strict';
+
+import test from 'ava';
+import BinarySearchTree from './BST';
+
+test('BST instance is a class of BinarySearchTree ', (t) => {
+  let node = new BinarySearchTree(1);
+
+  t.is(node instanceof BinarySearchTree, true);
+});
+
+test('BST instance has insert, contains', (t) => {
+  let {insert, contains} = new BinarySearchTree(1);
+
+  t.is(typeof insert, 'function');
+  t.is(typeof contains, 'function');
+});
+
+test('BST first node set value correctly when created', (t) => {
+  let node = new BinarySearchTree(1);
+
+  t.is(node.value, 1);
+});
+
+test('BST first node set value correctly when created', (t) => {
+  let node = new BinarySearchTree(1);
+
+  t.is(node.value, 1);
+});
+
+test('BST:insert:  first node, inserts right node correctly', (t) => {
+  let node = new BinarySearchTree(2);
+
+  node.insert(3);
+
+  t.is(node.right.value, 3);
+});
+
+test('BST:insert first node, inserts left node correctly', (t) => {
+  let node = new BinarySearchTree(2);
+
+  node.insert(1);
+
+  t.is(node.left.value, 1);
+});
+
+test('BST:insert a third value should we added to the left node', (t) => {
+  let node = new BinarySearchTree(2);
+
+  node.insert(1);
+  node.insert(3);
+  node.insert(0);
+
+  t.is(node.left.left.value, 0);
+});
+
+test('BST:contains: values should be found in the tree', (t) => {
+  let node = new BinarySearchTree(2);
+
+  node.insert(1);
+  node.insert(3);
+  node.insert(0);
+
+  t.is(node.contains(2), true);
+  t.is(node.contains(1), true);
+  t.is(node.contains(3), true);
+});
+
+test('BST:contains: value should not be found in the tree', (t) => {
+  let node = new BinarySearchTree(2);
+
+  node.insert(1);
+  node.insert(3);
+  node.insert(0);
+
+  t.is(node.contains(5), false);
+});
